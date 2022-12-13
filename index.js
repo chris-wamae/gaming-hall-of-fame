b01aae1e525739dff1df02935dae22b654fbb09e
chris-wamae <chriswamae123@gmail.com>
2022-12-13 18:34:27 +0300

    Added functionality to hide and display the sign up form when its needed

--- index.js
+++ index.js
@@ -3,3 +3,21 @@ fetch("https://valorant-api.com/v1/playercards")
 .then(cardsData => {console.log(cardsData.data[0]["displayIcon"])
                let playerCard1 = document.querySelector("img")
                 playerCard1.setAttribute("src",`${cardsData.data[0]["displayIcon"]}`)})
+
+function formDisplay(){
+document.querySelector("section#sign-up").style.display = "none";
+let signUp = document.querySelector("p#sign-up-nav")
+signUp.addEventListener("click",function(){
+    document.querySelector("section#sign-up").setAttribute("style","")
+let signUpSubmit = document.querySelector("button#sign-up-button")
+signUpSubmit.addEventListener("click",function(event){
+    event.preventDefault()
+    document.querySelector("section#sign-up").style.display = "none"
+    alert("sign up successful")
+
+})
+})
+}
+formDisplay()
+
+
