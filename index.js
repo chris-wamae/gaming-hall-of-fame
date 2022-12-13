d774952810f16d27f03afd660098f2a0ca5a1e1f
chris-wamae <chriswamae123@gmail.com>
2022-12-13 20:50:39 +0300

    added menu section and styled it

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 fetch("https://valorant-api.com/v1/playercards")
-.then(response => response.json())
-.then(cardsData => {console.log(cardsData.data[0]["displayIcon"])
-               let playerCard1 = document.querySelector("img")
-                playerCard1.setAttribute("src",`${cardsData.data[0]["displayIcon"]}`)})
+  .then((response) => response.json())
+  .then((cardsData) => {
+    console.log(cardsData.data);
+    let playerCard1 = document.querySelector("img");
+    playerCard1.setAttribute("src", `${cardsData.data[223]["displayIcon"]}`);
+  });
 
-function formDisplay(){
-document.querySelector("section#sign-up").style.display = "none";
-let signUp = document.querySelector("p#sign-up-nav")
-signUp.addEventListener("click",function(){
-    document.querySelector("section#sign-up").setAttribute("style","")
-let signUpSubmit = document.querySelector("button#sign-up-button")
-signUpSubmit.addEventListener("click",function(event){
-    event.preventDefault()
-    document.querySelector("section#sign-up").style.display = "none"
-    alert("sign up successful")
-
-})
-})
+function formDisplay() {
+  document.querySelector("section#sign-up").style.display = "none";
+  let signUp = document.querySelector("p#sign-up-nav");
+  signUp.addEventListener("click", function () {
+    document.querySelector("section#sign-up").setAttribute("style", "");
+    let signUpSubmit = document.querySelector("button#sign-up-button");
+    signUpSubmit.addEventListener("click", function (event) {
+      event.preventDefault();
+      document.querySelector("section#sign-up").style.display = "none";
+    });
+  });
 }
-formDisplay()
-
-
+formDisplay();
+function submitAlert() {
+  function alertTimeout() {
+    alert("Sign up successful");
+  }
+  setTimeout(alertTimeout, 10);
+}
