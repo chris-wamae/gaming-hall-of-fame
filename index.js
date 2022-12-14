46fa27b844b97b8f2e9089c8008ba1e5698d6548
chris-wamae <chriswamae123@gmail.com>
2022-12-14 19:19:13 +0300

    created form for adding players to the Hall of Fame

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-
-function fetchData(callBack){
-fetch("https://valorant-api.com/v1/playercards")
-  .then((response) => response.json())
-  .then((cardsData) => callBack(cardsData["data"]))
+function fetchData(callBack) {
+  fetch("https://valorant-api.com/v1/playercards")
+    .then((response) => response.json())
+    .then((cardsData) => callBack(cardsData["data"]));
 }
 
 function formDisplay() {
@@ -26,24 +25,76 @@ function submitAlert() {
   setTimeout(alertTimeout, 10);
 }
 //selects the next Image when choosing an avatar
-let firstImage = 0
-function imageSelection(apiArray){
-  console.log(apiArray)
-let nextImage = document.querySelector("button#next-image")
-nextImage.addEventListener("click",function(){
-let currentImage = document.querySelector("img#player-avatar")
-currentImage.setAttribute("src",apiArray[firstImage]["displayIcon"])
-firstImage += 1})
 
-let previousImage = document.querySelector("button#previous-image")
-previousImage.addEventListener("click",function(){
-let currentImage = document.querySelector("img#player-avatar")
-currentImage.setAttribute("src",apiArray[firstImage]["displayIcon"])
-if(firstImage > 0){
-firstImage += -1
-}})
+function imageSelection(apiArray) {
+  let firstImage = 0;
+  let currentImage = document.querySelector("img#player-avatar");
+  currentImage.setAttribute("src", apiArray[firstImage]["displayIcon"]);
+  console.log(apiArray);
 
+  let nextImage = document.querySelector("button#next-image");
+  nextImage.addEventListener("click", function () {
+    firstImage = firstImage + 1;
+    currentImage.setAttribute("src", apiArray[firstImage]["displayIcon"]);
+  });
+
+  let previousImage = document.querySelector("button#previous-image");
+  previousImage.addEventListener("click", function () {
+    if (firstImage > 0) {
+      firstImage = firstImage - 1;
+    } else {
+      firstImage = 0;
+    }
+    currentImage.setAttribute("src", apiArray[firstImage]["displayIcon"]);
+  });
+
+  let selectImage = document.querySelector("button#select-image");
+  selectImage.addEventListener("click", function () {
+    let selectedImage = document.createElement("img");
+    selectedImage.setAttribute("src", apiArray[firstImage]["displayIcon"]);
+    let imageSection = document.querySelector("section#hall-of-fame");
+    imageSection.append(selectedImage);
+  });
 }
 
+fetchData(imageSelection);
 
-fetchData(imageSelection)
+function addName() {
+  let hallOfFameForm = document.querySelector("form#hall-of-fame-form");
+  hallOfFameForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    if (event.target.inGameName.value != "") {
+      let formInputs = document.querySelector("section#hall-of-fame");
+      let setInGameName = document.createElement("p");
+      setInGameName.textContent = event.target.inGameName.value;
+      formInputs.append(setInGameName);
+      let currentNames = document.querySelectorAll("section#hall-of-fame p");
+      for (let i = 0; i <= currentNames.length - 1; i++) {
+        for (let j = i + 1; j <= currentNames.length - 1; j++) {
+          if (currentNames[i].textContent === currentNames[j].textContent) {
+            currentNames[j].remove();
+          }
+        }
+      }
+    }
+  });
+} 
+addName();
+
+function addGame(){
+  let gamesList = document.querySelector("section#hall-of-fame")
+  let hallOfFameForm = document.querySelector("form#hall-of-fame-form");
+  hallOfFameForm.addEventListener("submit", function (event) {
+event.preventDefault()
+let checkedBoxes = document.querySelectorAll("input[type = checkbox]:checked")
+for(let checkedBox of checkedBoxes){
+ let box =  document.createElement("p")
+ box.textContent = checkedBox.value
+ gamesList.append(box)
+ 
+}
+
+}
+)
+}
+addGame()
